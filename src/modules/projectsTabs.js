c410ec263a32e27829c8dcbc23156fae7edf9b96
Alexandr <kuvaldin.ai95@gmail.com>
2021-03-07 03:14:43 +0700

    Правки + фильтр карточек на странице projects

---
+++ src/modules/projectsTabs.js
@@ -0,0 +1,42 @@
+  const projectsTabs = () => {
+
+  if(document.location.pathname !== '/projects.html') {
+    return;
+  }
+
+  const navMenu = document.querySelector('.nav-menu'),
+  navLi = navMenu.querySelectorAll('li>a'),
+  projectPage = document.querySelectorAll('.project-page');
+
+  const filter = (category, items) => {
+    
+    items.forEach(item => {
+      const itemFiltered = !item.classList.contains(category);
+      const isShowAll = category.toLowerCase() === 'all';
+
+      if(itemFiltered && !isShowAll) {
+        item.style.display = 'none'
+      } else{
+        item.style.display = 'block'
+      }
+    })
+  };
+
+    navLi.forEach(item => {
+
+      item.addEventListener('click', e => {
+        e.preventDefault();
+        let target = e.target;
+        navLi.forEach(i => i.classList.remove('nav-link_active'));
+        target.classList.add('nav-link_active')
+
+        filter(target.dataset.category, projectPage)
+
+      
+      });
+  });
+
+  
+};
+
+export default projectsTabs;
