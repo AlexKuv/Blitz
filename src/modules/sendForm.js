b404f807707e32a26d64e136a399914e14e644d2
Alexandr <kuvaldin.ai95@gmail.com>
2021-03-08 22:39:40 +0700

    Валидатор для формы

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -5,6 +5,41 @@ const sendForm = () => {
     }
 
   const formUser = document.querySelector('#form-user');
+  const formbtn = document.querySelector('.form-btn');
+
+  // валидатор input
+  let inputValid = formUser.querySelectorAll('input');
+      inputValid.forEach((elem) => {
+
+        elem.addEventListener('input', (e) => {
+
+          if(elem.matches('[type=email]')) {
+            let email = document.querySelector('[type=email]');
+            let reg = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+
+            if(!reg.test(email.value)){
+              email.style.cssText = `border: 1px solid red`;
+              formbtn.disabled = true;
+            }else if(reg.test(email.value)){
+              email.style.cssText = '';
+              formbtn.disabled = !reg.test(elem.value);
+            }
+          }
+          
+        if (elem.matches('[type=text]')){
+          elem.value = elem.value.replace(/[^а-яА-Яa-zA-Z]/,  '');
+        }
+        
+        
+          
+      });
+    });
+
+    // валидатор для textarea
+    let message = document.querySelector('.message')
+      message.addEventListener('input', () => {
+        message.value = message.value.replace(/[^а-яА-Яa-zA-Z\s\,\.\?\!\-\;\:]/,  '');
+      });
 
   formUser.addEventListener('submit', event => {
     event.preventDefault();
